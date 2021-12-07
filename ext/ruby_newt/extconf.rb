d67736e8d41b89f3dfc51a41c306a0719710eace
Robert Gill <rtgill82@gmail.com>
2021-12-06 19:35:30 -0800

    Specify additional CFLAGS and LDFLAGS from environment

--- ext/ruby_newt/extconf.rb
+++ ext/ruby_newt/extconf.rb
@@ -6,4 +6,6 @@ dir_config('newt')
 have_library('slang', 'SLsmg_refresh')
 have_library('newt', 'newtInit')
 
+append_cflags(ENV['CFLAGS'])
+append_ldflags(ENV['LDFLAGS'])
 create_makefile('ruby_newt/ruby_newt')
