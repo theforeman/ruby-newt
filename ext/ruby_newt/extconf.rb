eb29d9f08e572410c18718c4e042d73a7682de54
Barry Allard <steakknife@users.noreply.github.com>
2022-09-05 14:35:59 -0500

    Eliminates need for any magic flags
    Works OOTB

--- ext/ruby_newt/extconf.rb
+++ ext/ruby_newt/extconf.rb
@@ -1,11 +1,9 @@
 require 'mkmf'
 
-dir_config('slang')
-dir_config('newt')
-
-have_library('slang', 'SLsmg_refresh')
-have_library('newt', 'newtInit')
+pkg_config('s-lang')
+pkg_config(RUBY_PLATFORM.include?('darwin') ? 'libnewt' : 'newt')
 
 append_cflags(ENV['CFLAGS'])
 append_ldflags(ENV['LDFLAGS'])
+
 create_makefile('ruby_newt/ruby_newt')
